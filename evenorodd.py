da9e2d521077791c0b4fe13dfda01fcf3070f9f4
michellekim622 <62683110+michellekim622@users.noreply.github.com>
2020-04-22 15:11:07 -0400

    Create evenorodd.py

---
+++ evenorodd.py
@@ -0,0 +1,8 @@
+# Even or Odd 
+
+number = int(input("Please enter any integer number :"))
+
+if(number%2==0):
+  print("{0} is an Even number".format(number))
+else:
+  print("{0} is an Odd number".format(number))
