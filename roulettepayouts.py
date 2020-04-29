34d742ae14660d53086c69e450be2bbbd623b302
michellekim622 <62683110+michellekim622@users.noreply.github.com>
2020-04-28 23:39:03 -0400

    Create roulettepayouts.py

---
+++ roulettepayouts.py
@@ -0,0 +1,35 @@
+import random
+
+space = random.randint(0, 37)
+
+if space == 0:
+	if random.randint(0, 2) == 0:
+		print("Pay 00")
+	else:
+		print("Pay 0")
+
+# single number (1 to 36, 0, or 00)		
+else:
+	print("The spin resulted in {}...".format(space))
+
+	print("Pay {}".format(space))
+
+# Red versus Black
+	if space == 1 or space == 3 or space == 5 or space == 7 or space == 9 or space == 12 or space == 14 or space == 16 or space == 18 \
+	or space == 19 or space == 21 or space == 23 or space == 25 or space == 27 or space == 30 or space == 32 or space == 34 or space == 36:
+		print("Pay Red")
+	else:
+		print("Pay Black")
+
+# Odd versus Even (0 and 00 do not payout for even)		
+	if space > 0:
+		if space % 2 == 0:
+			print("Pay Even")
+		else:
+			print("Pay Odd")
+
+# 1 to 18 versus 19 to 36	
+	if space >= 1 and space <= 18:
+		print("Pay 1 to 18")
+	elif space >= 19 and space < 36:
+		print("Pay 19 to 36")
